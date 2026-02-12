6eaa5bcfa3789a8bb7a1481402bd59c55836999e
Shafi Akinropo <sakinropo@gmail.com>
2026-02-12 19:40:53 +0100

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -242,6 +242,7 @@ func setDynamicSwaggerConfig() {
 func getAllowedOrigins() string {
 	base := getBaseURL()
 	defaults := []string{
+		"https://betadaypos.vercel.app",
 		"http://localhost:3000",
 		"http://localhost:3001",
 		"http://localhost:5173",
